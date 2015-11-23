f7f11b4a0dcf7f02f701e3f5fdf38525e3086044
Paul Boyle <pboyle17@gmail.com>
2015-11-22 19:55:12 -0600

    finishing up

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 gem 'pry'
 gem 'sinatra'
+gem 'json'
+gem 'thin'
+gem 'em-websocket'
--- app.rb
+++ app.rb
@@ -1,7 +1,35 @@
 require 'bundler'
+# require 'sinatra/base'
 
 Bundler.require
 
-get '/' do
-  p 'Hello World!'
+
+EventMachine.run do
+  class App < Sinatra::Base
+    get '/' do
+      erb :home
+    end
+  end
+  App.run! :port => 3000
+end
+
+@clients = []
+
+EM:WebSocket.start(:host => '0.0.0.0', :port '3001') do |ws|
+  ws.onopen do |handshake|
+    @clients $lt;&lt; ws
+    ws.send 'Connected to #{handshake.path}.'
+  end
+
+  ws.onclose do
+    ws.send 'closed'
+    @clients.delete ws
+  end
+
+  ws.onmessage do |msg|
+    puts "recieved message: #{msg}"
+    @clients.each do |socket|
+      socket.send msg
+    end
+  end
 end
